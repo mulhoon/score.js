5ee12f682bef6ae0c29ac4ad57255721b7555a8f
Nic Mulvaney <nic@nicmulvaney.com>
2014-09-24 17:57:21 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# levelup.js
+
+**levelup.js** is a small (2kb) javascript module to manage player progress across gaming levels.
+
+[WORK IN PROGRESS]
+
+## Quick usage
+```
+var levelup = new LevelUp();
+levelup.score(123);
+
+var scorecard = levelup.get();
+```
+scorecard returns...
+```
+{
+	score:123						// current total score
+	title:"Soldier",				// current title
+	quote:"You're a true soldier"	// current quote
+	level:2,						// current level
+	levelscore:13,					// current level score
+	leveltotal:200,					// current level total
+	levelprogress:25, 				// percentage %
+	totalprogress:34, 				// percentage %
+	levelup: false,					// did we level up?
+	totallevels:10
+}
+```
+levelup.js comes with 10 levels as default, each 100 points apart. These can easily be replaced with custom levels in the setup below.
+
+## Setup
+
+```
+var scorecard = new LevelUp(
+	{
+		persistant:boolean			// uses localStorage
+		levels:{}					// custom levels
+		callback:function(){...}	// trigger when levelling up
+	}
+);
+
+```
+
+## Example
+Check out the demo 
---
+++ source/levelup.js
@@ -0,0 +1,141 @@
+/*! levelup.js ~ (c) 2014 Nic Mulvaney */
+
+(function(){
+	var _settings,
+	_defaults = {
+		level:0,
+		score:0,
+		levels:[
+			{
+				"checkmark": 0,
+				"status":"turtle",
+				"quote":"You're just a little newbie"
+			},
+			{
+				"checkmark": 50,
+				"status":"sheep",
+				"quote":"You're certainly not following the herd"
+			},
+			{
+				"checkmark": 150,
+				"status":"pig",
+				"quote":"Oh my god, smokey bacon!"
+			},
+			{
+				"checkmark": 300,
+				"status":"rabbit",
+				"quote":"Ok, I'll admit you're a speed deamon"
+			},
+			{
+				"checkmark": 600,
+				"status":"cat",
+				"quote":"Your powers are strong my friend"
+			},
+			{
+				"checkmark": 1200,
+				"status":"octopus",
+				"quote":"Your reach is far"
+			},
+			{
+				"checkmark": 2000,
+				"status":"bee",
+				"quote":"The busiest player i've ever seen"
+			},
+			{
+				"checkmark": 2500,
+				"status":"horse",
+				"quote":"Oh boy, you're a powerful stallion"
+			},
+			{
+				"checkmark": 3000,
+				"status":"gorilla",
+				"quote":"You're a true survivor, grrr!"
+			},
+			{
+				"checkmark": 4000,
+				"status":"dolphin",
+				"quote":"You've arrived! you will be known forever as a Legend"
+			}
+		],
+		callback:function(){}
+	};
+
+	var LevelUp = function(settings){
+		_settings = this._merge(_defaults, settings || {});
+	};
+
+	LevelUp.prototype = {
+			version: '0.0.1',
+
+			score: function(score){
+
+				var total = 0, next, current;
+				var levelCount = _settings.levels.length;
+
+				for(var level in _settings.levels){
+					next = _settings.levels[level];
+					if(score < next.checkmark){
+						current = _settings.levels[level-1];
+						break;
+					}else{
+						total += next.checkmark;
+					}
+				}
+				if(!current){
+					level = levelCount;
+					current = _settings.levels[levelCount-1];
+				}
+				// current = current ? current : _settings.levels[levelCount-1];
+
+				var levelscore = score - current.checkmark;
+				var leveltotal = next.checkmark - current.checkmark;
+
+				var levelprogress = (levelscore/leveltotal * 100).toFixed(2);
+				levelprogress = isFinite(levelprogress) ? levelprogress : 100;
+
+				var totalprogress = this._restrict(score/_settings.levels[levelCount-1].checkmark *100).toFixed(2);
+				var levelup = _settings.level!==level;
+				_settings.level = level;
+
+				var data = this._merge({
+					score:score,
+					level:level,
+					levelscore:levelscore,
+					levelprogress:levelprogress,
+					leveltotal:leveltotal,
+					totalprogress:totalprogress,
+					levelup:levelup,
+					totallevels:_settings.levels.length
+				}, current);
+
+				if(levelup){
+					_settings.callback(data);
+				}
+				return data;
+
+			},
+			_restrict: function(value){
+				return Math.max(Math.min(value, 100), 0);
+			},
+			_merge: function(obj1, obj2){
+				for (var key in obj2){ 
+					obj1[key] = obj2[key]; 
+				}
+				return obj1;
+			},
+			_save: function(){
+				return localStorage.saveItem("levelupscore", _scoreboard);
+			},
+			_get: function(){
+				return localStorage.getItem("levelupscore");
+			}
+	};
+
+
+	if ( typeof module != 'undefined' && module.exports ) {
+		module.exports = LevelUp;
+	} else {
+		window.LevelUp = LevelUp;
+	}
+
+})();
